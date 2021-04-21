c6b28db6d99a90e3ed8a778d9c886161fd81e8f1
prince <princeakpabio90@gmail.com>
2021-04-22 00:56:30 +0100

    API Consumption: consumed update account API

--- src/components/account/account-actions/account-list.jsx
+++ src/components/account/account-actions/account-list.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { Element } from "react-scroll";
 import { BASE_API_URL } from "../../../hooks/API";
 import CustomTableList from "../../general/custom-table-list/custom-table-list";
+import UpdateAccountForm from "../../fuel-issues/add-Fuel-Form";
+import WidgetHeader from "../../general/widget-header";
 
 const AccountList = () => {
   const [accountList, setAccountList] = useState();
+  const [chartList, setChartList] = useState();
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
   useEffect(() => {
     const source = axios.CancelToken.source();
     const response = async () => {
@@ -28,6 +33,12 @@ const AccountList = () => {
                 const account_id = item.id;
                 const chart_id = item.chart_id;
                 const description = item.description;
+
+                const updateData = {
+                  account_name: account_name,
+                  account_id: account_id,
+                  description: item.description,
+                };
 
                 const currentAccountItem = {
                   id: account_id,
@@ -53,7 +64,9 @@ const AccountList = () => {
                       editScroll: true,
                       scrollLocation: "update-form",
                       onClick: () => {
-                        // handleUpdateFormFields(productItemData);
+                        handleUpdateForm(updateData);
+                        // setShowUpdateForm((prev) => !prev);
+                        setShowUpdateForm(true);
                       },
                     },
                     {
@@ -97,6 +110,71 @@ const AccountList = () => {
       source.cancel();
     };
   }, []);
+
+  /**
+   * Chart API call for the select dropdown
+   *  */
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const response = async () => {
+      let chartListBody = [];
+      try {
+        await axios
+          .get(`${BASE_API_URL}/api/v1/account/chart-list.php`)
+          .then((res) => {
+            console.log("Chart list response data: ", res.data);
+            if (res.data.error) {
+              let title = "Server Error Response",
+                text = res.data.message;
+              errorAlert(title, text);
+            } else {
+              const chartListItems = res.data.data;
+              chartListItems.map((item) => {
+                const account_id = parseInt(item.id),
+                  account_type = item.account_type,
+                  statement = item.statement,
+                  statement_id = item.statement_id,
+                  description = item.description;
+
+                const currentChartItem = {
+                  id: account_id,
+                  description: description,
+                  account_type: account_type,
+                  statement: statement,
+                  statement_id: statement_id,
+                };
+
+                return (chartListBody = chartListBody.concat(currentChartItem));
+              });
+              chartListBody.unshift({
+                id: 0,
+                account_type: "Select a chart Type",
+              });
+              setChartList(chartListBody);
+              console.log("Chart List Body: ", chartList);
+            }
+          })
+          .catch((error) => {
+            console.log("API error: ", error);
+            let title = "Network Error",
+              text = error;
+            errorAlert(title, text);
+          });
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Axios Error: ", error);
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    response();
+
+    return () => {
+      source.cancel();
+    };
+  }, []);
   /** Multipurpose success, error and warning pop-ups for handling and displaying errors, success and warning alerts */
   const successAlert = (title, text, link) => {
     Swal.fire({
@@ -125,7 +203,45 @@ const AccountList = () => {
       }
     });
   };
+  const handleUpdateAccount = () => {
+    const userDetails = JSON.parse(localStorage.getItem("user")),
+      user_name = userDetails.username,
+      user_id = userDetails.id;
+    const account = document.getElementById("account-name").value;
+    const account_id = document.getElementById("account-id").value;
+    const description = document.getElementById("account-description").value;
+    const chartValue = parseInt(document.getElementById("chart-id").value);
+    const chartItem = chartList.filter(({ id }) => id === chartValue);
 
+    console.log("chart item: ", chartItem);
+    const updateAccountData = {
+      user: user_name,
+      "user-id": user_id,
+      "account-id": account_id,
+      account: account,
+      "chart-id": chartValue,
+      description: description,
+    };
+    console.log("Update API values: ", updateAccountData);
+    axios
+      .post(
+        `${BASE_API_URL}/api/v1/account/account-update.php`,
+        updateAccountData
+      )
+      .then((res) => {
+        console.log("Update account response data: ", res.data);
+        if (res.data.error) {
+          let title = "Server Error Response",
+            text = res.data.message;
+          errorAlert(title, text);
+        } else {
+          let title = "Account updated Successfully",
+            text = res.data.message,
+            link = `<a href="/accountlist">View Account List</a>`;
+          successAlert(title, text, link);
+        }
+      });
+  };
   const handleDeleteAccount = (id) => {
     const userDetails = JSON.parse(localStorage.getItem("user")),
       user_name = userDetails.username,
@@ -151,6 +267,15 @@ const AccountList = () => {
       });
   };
 
+  const handleUpdateForm = (updateData) => {
+    if (document.getElementById("account-id") !== null) {
+      document.getElementById("account-id").value = updateData.account_id;
+      document.getElementById("account-name").value = updateData.account_name;
+      document.getElementById("account-description").value =
+        updateData.description;
+    }
+  };
+
   /** Account List Table Data */
   const accountListTableData = {
     tableTitle: "Account List",
@@ -164,10 +289,69 @@ const AccountList = () => {
 
     body: accountList,
   };
+  const updateAccountFormData = [
+    {
+      id: "chart-id",
+      type: "select",
+      name: "chart",
+      holder: "",
+      className: "form-control",
+      options: chartList,
+      required: true,
+    },
+    {
+      id: "account-id",
+      type: "text",
+      name: "account-id",
+      holder: "Account ID",
+      className: "form-control",
+      hidden: true,
+      required: true,
+    },
+    {
+      id: "account-name",
+      type: "text",
+      name: "account",
+      holder: "Account Name",
+      className: "form-control",
+      required: true,
+    },
+    {
+      id: "account-description",
+      type: "text",
+      name: "description",
+      holder: "Account description",
+      className: "form-control",
+      required: true,
+    },
+  ];
+
   console.log(accountListTableData);
   /** Account list component display */
   const AccountListComponent = () => (
-    <CustomTableList content={accountListTableData} />
+    <div id="basic" className="col-lg-12 layout-spacing">
+      <Element className="update-form" id="update-form" />
+      <div className="statbox widget box box-shadow">
+        {/* HEADER TITLE FOR THE FORM */}
+
+        {showUpdateForm && <WidgetHeader title="Update account" />}
+        <div
+          className="widget-content widget-content-area searchable-container list"
+          style={{ display: "grid", padding: "2rem", gap: "2rem" }}
+        >
+          {showUpdateForm && (
+            <UpdateAccountForm
+              content={updateAccountFormData}
+              // loading={loading}
+              subtitle="Update account information"
+              btnText="Update account"
+              handleAddSubmit={() => handleUpdateAccount()}
+            />
+          )}
+          <CustomTableList content={accountListTableData} />
+        </div>
+      </div>
+    </div>
   );
   return <AccountListComponent />;
 };
