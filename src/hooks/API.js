d7b5a3737ff86b942a69e147a1b34ccc503e249d
Abidemi <Anthony.adeniyi@yahoo.com>
2021-05-09 16:16:31 +0100

    API path changed to local for testing

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
-// export const BASE_API_URL = "http://localhost/mdredge-server";
+//export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+export const BASE_API_URL = "http://localhost/mdredge-server";
