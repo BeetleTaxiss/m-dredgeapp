37786463b5d290539f424ae6728c4d6ec9d1a472
Abidemi <Anthony.adeniyi@yahoo.com>
2021-05-17 12:37:51 +0100

    API server changed back to server.atupgroup.com

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,4 +1,3 @@
-//export const BASE_API_URL = "https://server.atupgroup.com";
-// export const BASE_API_URL = "https://server.atupgroup.com";
+ export const BASE_API_URL = "https://server.atupgroup.com";
 //export const BASE_API_URL = "http://localhost/mdredge-server";
-export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+//export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
