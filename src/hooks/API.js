179851951ea70d696664a0b01474b462ea283bb8
Abidemi <Anthony.adeniyi@yahoo.com>
2021-05-17 12:35:57 +0100

    API PUT calls changed to POST

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,4 +1,3 @@
 //export const BASE_API_URL = "https://server.atupgroup.com";
 export const BASE_API_URL = "https://server.atupgroup.com";
 //export const BASE_API_URL = "http://localhost/mdredge-server";
-
