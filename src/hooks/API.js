b4c91d17c3b9a0d1492b3d652971a8da0288535b
Abidemi <Anthony.adeniyi@yahoo.com>
2021-05-21 13:02:34 +0100

    Development and Production app now run two distinct servers

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,8 +1,19 @@
+
 /**
- * Product Server API
+ * The Local test and development server. This is purely for local 
+ * testing of our application
  */
-// export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+//export const BASE_API_URL = "http://localhost/mdredge-server";
+
 /**
- * Development Server API
+ * The actual production server of our application. Before we push to 
+ * the master (Live Site) we must uncomment this and comment out the 
+ * development section below so that app will connect to the live version
  */
-export const BASE_API_URL = "https://mdredge-server-api-dev.herokuapp.com";
+//export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+
+/** 
+ * The developement server. This is the server that runs the development version 
+ * of our application. We must uncooment this before publishing to development
+ * */
+ export const BASE_API_URL = "https://mdredge-server-api-dev.herokuapp.com";
