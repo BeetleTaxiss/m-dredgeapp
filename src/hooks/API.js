d15f4767081cfee438e7e00c965b2fbf0a79de44
prince <princeakpabio90@gmail.com>
2021-05-24 10:17:59 +0100

    changed API base link to development server

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,8 +1,8 @@
 /**
  * Product Server API
  */
-export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+// export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
 /**
  * Development Server API
  */
-// export const BASE_API_URL = "https://mdredge-server-api-dev.herokuapp.com";
+export const BASE_API_URL = "https://mdredge-server-api-dev.herokuapp.com";
