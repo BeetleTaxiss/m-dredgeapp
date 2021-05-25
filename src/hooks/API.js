e7f6e52afa73a6149006ba74991a156a2bbf57ff
prince <princeakpabio90@gmail.com>
2021-05-25 11:58:47 +0100

    Change of API base link to production server

--- src/hooks/API.js
+++ src/hooks/API.js
@@ -2,14 +2,14 @@
  * The Local test and development server. This is purely for local
  * testing of our application
  */
-export const BASE_API_URL = "http://localhost/mdredge-server";
+// export const BASE_API_URL = "http://localhost/mdredge-server";
 
 /**
  * The actual production server of our application. Before we push to
  * the master (Live Site) we must uncomment this and comment out the
  * development section below so that app will connect to the live version
  */
-// export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
+export const BASE_API_URL = "https://mdredge-server-api.herokuapp.com";
 
 /**
  * The developement server. This is the server that runs the development version
