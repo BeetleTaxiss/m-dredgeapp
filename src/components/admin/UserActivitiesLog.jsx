7572a77728151e30ee55c9d0eedc5d0788f86d14
prince <princeakpabio90@gmail.com>
2021-06-18 11:26:01 +0100

    Pagination: Started implementing pagination for user activities log

--- src/components/admin/UserActivitiesLog.jsx
+++ src/components/admin/UserActivitiesLog.jsx
@@ -11,10 +11,17 @@ import {
   PaginationManager,
   Paginator,
 } from "../../hooks/paginator";
-import { useGetUserDetails } from "../../hooks/function-utils";
+import {
+  productDropdownForTable,
+  useGetUserDetails,
+  validateProductLocationPermission,
+} from "../../hooks/function-utils";
 
 const UserActivitiesLog = () => {
   const [userActivitiesLog, setUserActivitiesLog] = useState(["loading"]);
+  const [userPermissions, setUserPermissions] = useState();
+  const [userProductPermission, setUserProductPermission] = useState();
+  const [productId, setProductId] = useState();
 
   // Table item count and last item id from db (State)
   const [listCount, setListCount] = useState("5");
@@ -32,6 +39,61 @@ const UserActivitiesLog = () => {
   // Tracks if an item has been deleted from the table and sets it to true
   let newDataFetch = useRef(false);
 
+  /**
+   * Optional paramaters not needed in the useGetUserDetails hook
+   */
+  const optionalParams = ["d", "7", "d", "7", "s", "w"];
+
+  /*
+   * Get user data from user store with custom hook and subscribe the state values to a useEffect to ensure   * delayed async fetch is accounted for
+   */
+  useGetUserDetails(...optionalParams, setUserPermissions);
+
+  /**
+   * Fetch Product list from database and validate per user
+   */
+  useEffect(() => {
+    axios
+      .get(`${BASE_API_URL}/api/v1/product/list.php`)
+      .then((res) => {
+        if (res.data.error) {
+          errorAlert("Server Error Response", res.data.message);
+        } else {
+          let data = res.data.data;
+          /**
+           * Validated product data that is derived from a user's product permisssion
+           */
+          let validatedProductData;
+
+          /**
+           * This block ensures the validateProductLocationPermission utility is run when the user permission state hasn't be updated with actual data
+           */
+          if (userPermissions !== undefined || userPermissions !== null) {
+            /**
+             * utility function takes in a users permission and the product list from the database and validates what product permission the user has
+             */
+            validatedProductData = validateProductLocationPermission(
+              userPermissions?.productPermissions,
+              data
+            );
+
+            /**
+             * Set the validated product to state to make it globally accessiable
+             */
+            const tableDropdown = productDropdownForTable(
+              validatedProductData,
+              setProductId
+            );
+
+            setUserProductPermission(tableDropdown);
+          }
+        }
+      })
+      .catch((error) => {
+        errorAlert("Network Error", error);
+      });
+  }, [userPermissions, productId]);
+
   // Fetch User activity logs from db
   useEffect(() => {
     const source = axios.CancelToken.source();
@@ -45,6 +107,10 @@ const UserActivitiesLog = () => {
           params: {
             count: "10",
             "last-item-id": lastItemId,
+            "product-id":
+              productId === undefined && userProductPermission !== undefined
+                ? userProductPermission[0]?.id
+                : productId,
           },
         })
         .then((res) => {
@@ -143,12 +209,15 @@ const UserActivitiesLog = () => {
         });
     };
 
-    response();
+    /**
+     * Run axios call when product Id is valid and retrived
+     */
+    userPermissions !== undefined && response();
 
     return () => {
       source.cancel();
     };
-  }, [lastItemId]);
+  }, [userProductPermission, lastItemId]);
 
   // Update state values dynamically
   useEffect(() => {}, [
@@ -176,9 +245,11 @@ const UserActivitiesLog = () => {
       showConfirmButton: false,
     });
   };
+
   /** Detailed Log List Table Data */
   const detailedOldLogListTableData = {
     tableTitle: "Users Action Log",
+    links: userProductPermission,
     header: [
       { class: "", title: "Date" },
       { class: "", title: "Action Taken" },
@@ -224,6 +295,8 @@ const UserActivitiesLog = () => {
           setSearchBoxValue
         );
       }}
+      dropdown
+      change
       search
       footer
       {...footerProp}
